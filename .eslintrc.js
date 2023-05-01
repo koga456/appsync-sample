e60b45c63d65a04fc4e33d3566e7cb2b07fdd5ff
koga456 <yutakoga1002@gmail.com>
2023-05-01 19:33:44 +0900

    Added eslint and prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'prettier',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js', '*.d.ts'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        'jest/expect-expect': 'off',
+    },
+};
